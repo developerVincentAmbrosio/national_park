344f315483f1ea3ab64534dd76e353733dbf26a3
developerVincentAmbrosio <70174185+developerVincentAmbrosio@users.noreply.github.com>
2020-11-17 14:25:21 -0500

    Add files via upload
    made changes per grader

--- index.js
+++ index.js
@@ -5,36 +5,33 @@ const apiKey = "bqe6OTlEglcZAXClrTLUPwHffKkd9cn9ixqtuBkU";
 
 function formatQueryParams(params) {
     const queryItems = Object.keys(params)
-        .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+        //array[PA, NJ]
+        //stateCode=PA&stateCode=NJ
+        .map(key => {
+            if (key === "stateCode") {
+                let multipleStates = "";
+                params[key].forEach((state,i) => {
+                    if (i < params[key].length -1) {
+                        multipleStates += `${encodeURIComponent(key)}=${encodeURIComponent(state.trim())}&`    
+                    } else {
+                        multipleStates += `${encodeURIComponent(key)}=${encodeURIComponent(state.trim())}`
+                    }
+                })
+                console.log(multipleStates);
+                return multipleStates;
+            } else {
+                return `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`;
+            }
+        })
     return queryItems.join('&');
 }
 
-function displayParkNamesDescriptions(responseJson) {
-    console.log(responseJson.data);
-    $('.js-results-list').empty();
-    for (let i = 0; i < responseJson.data.length; i++) {
-//        for (let j = 0; j < responseJson.data.length; j++) {
-//            const parkAddress = `${responseJson.data[i].addresses[j].type} Address: ${responseJson.data[i].addresses[j].line1} ${responseJson.data[i].addresses[j].line2} ${responseJson.data[i].addresses[j].city} ${responseJson.data[i].addresses[j].stateCode} ${responseJson.data[i].addresses[j].postalCode}`;
-            
-//            console.log(parkAddress);
-//        }
-        $('.js-results-list').append(`
-            <ul class="item">
-                <li><h3>${responseJson.data[i].fullName}</h3></li>
-                <li><a href="${responseJson.data[i].url}">${responseJson.data[i].url}</a></li>
-                <li><p>${responseJson.data[i].description}</p></li>
-            </ul>`) //<li>${parkAddress}</li>
-        };
-    $('.js-results').removeClass('hidden');
-}
-
-function getParkInfo(stateCode, maxResults=10) {
+function getParkInfo(query) {
     const params = {
-        api_key: apiKey,
-        stateCode: stateCode.trim().split(",") ,
-        limit: maxResults
-    };
-
+        stateCode: query,
+        api_key: apiKey
+    }; 
+    
     const queryString = formatQueryParams(params)
     const url = baseUrl + '?' + queryString;
 
@@ -43,12 +40,33 @@ function getParkInfo(stateCode, maxResults=10) {
         .then(responseJson => displayParkNamesDescriptions(responseJson));
 }
 
+function displayParkNamesDescriptions(responseJson) {
+    console.log(responseJson.data);
+    $('.js-results-list').empty();
+    for (let i = 0; i < responseJson.data.length; i++) {
+//        for (let j = 0; j < responseJson.data.addresses.length; j++) {
+//            const parkAddress = `${responseJson.data[i].addresses[j].type} Address: ${responseJson.data[i].addresses[j].line1} ${responseJson.data[i].addresses[j].line2} ${responseJson.data[i].addresses[j].city} ${responseJson.data[i].addresses[j].stateCode} ${responseJson.data[i].addresses[j].postalCode}`;        
+//            console.log(parkAddress);
+//        }
+        $('.js-results-list').append(`
+            <ul class="item">
+                <li><h3>${responseJson.data[i].fullName}</h3></li>
+                <li><a href="${responseJson.data[i].url}" target="_blank">${responseJson.data[i].url}</a></li>
+                <li><p>${responseJson.data[i].description}</p></li>
+                
+                <hr>
+            </ul>`) // <li>${parkAddress}</li>
+        };
+    $('.js-results').removeClass('hidden');
+}
+
 function submitFormListener() {
     $('#submit').click(event => {
         event.preventDefault();
-        const userParkSearch = $('#js-search').val();
-        const maxResults = $('#js-max-results').val();
-        getParkInfo(userParkSearch, maxResults);
+        const userParkSearch = $('#js-search').val().trim().split(",");
+        const userNumberOfResults = $('#js-max-results').val();
+        getParkInfo(userParkSearch, userNumberOfResults);
+        console.log(userParkSearch);
     });
 }
 
